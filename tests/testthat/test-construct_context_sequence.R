642ae825ddbf6840c147a7afb2626169fb80e02e
Lang <franlang@tronlogin.zdv.uni-mainz.de>
2021-09-07 20:50:10 +0200

    remove test from removed junc

--- tests/testthat/test-construct_context_sequence.R
+++ tests/testthat/test-construct_context_sequence.R
@@ -1,31 +1,3 @@
-test_that("map_junc_transcript works", {
-  #junc_id <- "chr2_152727125_152728931_-"
-  junc_id <- "chr17_41152048_41152092_+"
-  junc_df <- tibble(junc_id = junc_id) %>%
-    separate(
-      junc_id,
-      sep = "_",
-      into = c("chr", "pos1", "pos2", "strand"),
-      remove = FALSE
-    ) %>%
-    mutate(pos1 = as.integer(pos1), pos2 = as.integer(pos2))
-  junc_pos1 <-
-    GenomicRanges::GRanges(junc_df$chr, junc_df$pos1, strand = junc_df$strand)
-  junc_pos2 <-
-    GenomicRanges::GRanges(junc_df$chr, junc_df$pos2, strand = junc_df$strand)
-  transcripts_covering_junction <-
-    map_junc_transcript(
-      junc_id = junc_id,
-      junc_pos1 = junc_pos1,
-      junc_pos2 = junc_pos2,
-      junc_df = junc_df,
-      transcript_db = toy_transcripts
-    )
-  expect_true(nrow(transcripts_covering_junction) == 6)
-  expect_true(ncol(transcripts_covering_junction) == 8)
-  expect_true(transcripts_covering_junction$enst[1] == "ENST00000589037")
-})
-
 test_that("construct_mutated_range works for A3SS event on positive strand", {
   junc_id <- "chr2_158156177_158157190_+"
   junc_df <- tibble(junc_id = junc_id) %>%
