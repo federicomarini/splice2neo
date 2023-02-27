3b0b73b561ddfb5fab07808311fca4bad4ae8046
Jonas Ibn-Salem <jonas.ibn-salem@tron-mainz.de>
2023-02-27 15:26:35 +0100

    add a unit test for empty input to format_pangolin()

--- tests/testthat/test-format_pangolin.R
+++ tests/testthat/test-format_pangolin.R
@@ -9,3 +9,16 @@ test_that("format_pangolin works on pangolin example file", {
   expect_true(all(df$score > 0))
 
 })
+
+test_that("format_pangolin works on empty input data.frame", {
+
+  pangolin_file <- system.file("extdata", "spliceai_output.pangolin.vcf", package = "splice2neo")
+  pangolin_df <- parse_pangolin(pangolin_file)
+  pangolin_empty <- pangolin_df[FALSE,]
+
+  df <- format_pangolin(pangolin_empty)
+
+  expect_true(nrow(df) == 0)
+
+
+})
